25287119b4ade85559f3c706f61e2bf6cb7c0a77
DamianoL <damianolunardo@hotmail.it>
2023-10-16 15:14:27 +0200

    ADD colored cell and logic for bombs

--- grid.js
+++ grid.js
@@ -2,6 +2,7 @@
 const grid = document.querySelector('.grid');
 const playButton = document.getElementById('btn-play');
 const difficultySelect = document.getElementById('select');
+const bombe_100 = getArrayOfRandomIntBetween(1, 100, 16);
 
 // quando clicco play
 playButton.addEventListener('click', function () {
@@ -46,11 +47,40 @@ cell.className += ' cell-49';
 
 // clicchi su una casella, cambia il suo colore 
 cell.addEventListener('click', function () {
+if (bombe_100.includes(i + 1)) {
+cell.style.backgroundColor = 'red';
+console.log("Hai cliccato su una bomba! La partita è finita.");
+} else {
 cell.style.backgroundColor = 'aqua';
 console.log("Hai cliccato sulla casella numero", i + 1);
+}
 });
 }
 });
 
+//per caso dificoltà normale
+function getArrayOfRandomIntBetween(minRange, maxRange, number) {
+const bombsArray = []
+    
+// popolare l'array con 16 numeri random non duplicati
+while (bombsArray.length < number) {
+// generare un numero random da rangeMin a rangeMAx
+const n = getRandomIntInclusive(minRange, maxRange)
 
+// SE n non è presente nell'array di bombe
+console.log(bombsArray.includes(n))
+if (bombsArray.includes(n) === false) {
+// pushare il numero nell'array di bombe
+bombsArray.push(n)
+}
+}
+    return bombsArray
+};
+
+ // generare un numero random da rangeMin a rangeMAx   
+function getRandomIntInclusive(min, max) {
+min = Math.ceil(min)
+max = Math.floor(max)
+return Math.floor(Math.random() * (max - min + 1) + min) 
+};
 
